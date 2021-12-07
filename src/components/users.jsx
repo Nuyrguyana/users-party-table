f39ed40509288c8fc59f8d102bbfe2519bc3f3d2
MichaelD <enee.kors@mail.ru>
2021-12-07 15:59:09 +0300

    create users table by React

---
+++ src/components/users.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import api from "../api";
+
+const Users = () => {
+    const [users, setUsers] = useState(api.users.fetchAll())
+    const handleDelete = (users) => {
+    }
+    const renderPhrase = (number) => {
+    }
+    return (
+        <>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th scope="col">Профессия</th>
+                    <th scope="col">Встретился, раз</th>
+                    <th scope="col">Оценка</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map((user) =>
+                    <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.qualities.map((quality) => {
+                            return <span className={'badge bg-' + quality.color}>{quality.name}</span>
+                        })}</td>
+                        <td>{user.profession.name}</td>
+                        <td>{user.completedMeetings}</td>
+                        <td>{user.rate}</td>
+                        <td>
+                            <button className='badge bg-danger'
+                                    // onClick={handleDelete}>
+                                >Delete
+                            </button>
+                        </td>
+                    </tr>)}
+                </tbody>
+            </table>
+        </>
+    )
+}
+export default Users
+
