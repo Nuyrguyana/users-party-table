1afee41c0942d0c2ad661c6830ef1836f56695a7
MichaelD <enee.kors@mail.ru>
2021-12-08 01:17:40 +0300

    delete table when users.length === 0

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -22,45 +22,47 @@ const Users = () => {
     const renderPhrase = () => {
         setCount((prevState) => prevState - 1)
     }
-    // const handleClick = (e) => {
-    //     const { renderPhrase, handleDelete} = this.props;
-    //
-    //     renderPhrase(e);
-    //     handleDelete(e);
-    // };
+    const renderUsersTable = () => {
+       return <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Встретился, раз</th>
+                <th scope="col">Оценка</th>
+            </tr>
+            </thead>
+            <tbody>
+            {users.map((user) =>
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.qualities.map((quality) => {
+                        return <span className={'badge m-1 bg-' + quality.color}>{quality.name}</span>
+                    })}</td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate}</td>
+                    <td>
+                        <button className='badge bg-danger'
+                                onClick={ () => handleDelete(user)}
+                        >
+                            Delete
+                        </button>
+                    </td>
+                </tr>)}
+            </tbody>
+        </table>
+    }
+
+    if (users.length === 0) {
+        return <span className={getBageClasses()}>{formatCount()}</span>
+
+    }
     return (
         <>
             <span className={getBageClasses()}>{formatCount()}</span>
-            <table className="table">
-                <thead>
-                <tr>
-                    <th scope="col">Имя</th>
-                    <th scope="col">Качества</th>
-                    <th scope="col">Профессия</th>
-                    <th scope="col">Встретился, раз</th>
-                    <th scope="col">Оценка</th>
-                </tr>
-                </thead>
-                <tbody>
-                {users.map((user) =>
-                    <tr key={user.id}>
-                        <td>{user.name}</td>
-                        <td>{user.qualities.map((quality) => {
-                            return <span className={'badge m-1 bg-' + quality.color}>{quality.name}</span>
-                        })}</td>
-                        <td>{user.profession.name}</td>
-                        <td>{user.completedMeetings}</td>
-                        <td>{user.rate}</td>
-                        <td>
-                            <button className='badge bg-danger'
-                                    onClick={ () => handleDelete(user)}
-                            >
-                                Delete
-                            </button>
-                        </td>
-                    </tr>)}
-                </tbody>
-            </table>
+            {renderUsersTable()}
         </>
     )
 }
