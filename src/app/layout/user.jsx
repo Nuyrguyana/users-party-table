417a290f1d45ef98130ce73ae78e1fb0eceb82a0
MichaelD <enee.kors@mail.ru>
2022-01-09 22:19:10 +0300

    improve user's card

--- src/app/layout/user.jsx
+++ src/app/layout/user.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../api';
+import Quality from '../components/qualitie';
 const User = ({ id }) => {
     console.log(id);
     const [user, setUser] = useState();
@@ -20,10 +21,10 @@ const User = ({ id }) => {
     if (user) {
         return <>
             <h2>{user.name}</h2>
-            <h3>user profession</h3>
-            <h4>user qualities</h4>
-            <h5>user completedMeeting</h5>
-            <h3>user rate</h3>
+            <h3>{user.profession.name}</h3>
+            {user.qualities.map((qual) => (<Quality key={qual._id} {...qual} />))}
+            <h5>{user.completedMeetings}</h5>
+            <h3>{user.rate}</h3>
             <button onClick={() => { handleAllUsers(); }}>
             Все пользователи
             </button>
