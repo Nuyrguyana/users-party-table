7db802f9492f6907f2199092ad678e2a42ff14d7
MichaelD <enee.kors@mail.ru>
2022-01-11 09:16:31 +0300

    add form for login

--- src/app/layout/login.jsx
+++ src/app/layout/login.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
+import React, { useState } from 'react';
 
-        </div>
-    );
+const Login = () => {
+    const [data, setData] = useState({ email: '', password: '' });
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    return (
+        <form action=''>
+            <div>
+                <label htmlFor='email'>Email</label>
+                <input type='text' id='email' name='email' value={data.email} onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor='password'>Password</label>
+                <input type='password' id='password' name='password' value={data.password} onChange={handleChange}/>
+            </div>
+        </form>);
 };
 
 export default Login;
