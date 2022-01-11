de614c0e8c31f88dffd9ae3484b233fa61bc4c18
MichaelD <enee.kors@mail.ru>
2022-01-11 10:42:14 +0300

    add submit and message error

--- src/app/layout/login.jsx
+++ src/app/layout/login.jsx
@@ -1,23 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import TextField from '../components/textField';
+import { validator } from '../utils/validator';
 
 const Login = () => {
     const [data, setData] = useState({ email: '', password: '' });
+    const [errors, setErrors] = useState({});
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }));
     };
+    const validatorConfig = {
+        email: { isRequired: { message: 'Электронная почта обязательна для заполнения' } },
+        password: { isRequired: { message: 'Пароль обязателен для заполнения' } }
+    };
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(data);
+    };
     return (
-        <form action=''>
-            <div>
-                <label htmlFor='email'>Email</label>
-                <input type='text' id='email' name='email' value={data.email} onChange={handleChange}/>
-            </div>
-            <div>
-                <label htmlFor='password'>Password</label>
-                <input type='password' id='password' name='password' value={data.password} onChange={handleChange}/>
-            </div>
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label='email'
+                name='email'
+                value={data.email}
+                onChange={handleChange}
+                error={errors.email}
+            />
+            <TextField
+                label='password'
+                type='password'
+                name='password'
+                value={data.password}
+                onChange={handleChange}
+                error={errors.password}
+            />
+            <button type='submit'>
+                Submit
+            </button>
         </form>);
 };
 
