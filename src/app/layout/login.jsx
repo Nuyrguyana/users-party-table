ae8b9fcefc6c27efdfdb16e1905fee048a9e9457
MichaelD <enee.kors@mail.ru>
2022-01-11 22:01:08 +0300

    add show/hide icone in password

--- src/app/layout/login.jsx
+++ src/app/layout/login.jsx
@@ -19,7 +19,13 @@ const Login = () => {
             }
         },
         password: {
-            isRequired: { message: 'Пароль обязателен для заполнения' }
+            isRequired: { message: 'Пароль обязателен для заполнения' },
+            isCapitalSymbol: { message: 'Пароль должен содержать хотя бы одну заглавную букву' },
+            isContainDigit: { message: 'Пароль должен содержать хотя бы одну цифру' },
+            min: {
+                message: 'Пароль должен состоять минимум из 8 символов',
+                value: 8
+            }
         }
     };
     useEffect(() => {
@@ -30,6 +36,7 @@ const Login = () => {
         setErrors(errors);
         return Object.keys(errors).length === 0;
     };
+    const isValid = Object.keys(errors).length === 0;
     const handleSubmit = (e) => {
         e.preventDefault();
         const isValid = validate();
@@ -37,26 +44,34 @@ const Login = () => {
         console.log(data);
     };
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField
-                label='email'
-                name='email'
-                value={data.email}
-                onChange={handleChange}
-                error={errors.email}
-            />
-            <TextField
-                label='password'
-                type='password'
-                name='password'
-                value={data.password}
-                onChange={handleChange}
-                error={errors.password}
-            />
-            <button type='submit'>
-                Submit
-            </button>
-        </form>);
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-md-6 offset-md-3 shadow p-4'>
+                    <h3 className='mb-4'>Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label='email'
+                            name='email'
+                            value={data.email}
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label='password'
+                            type='password'
+                            name='password'
+                            value={data.password}
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <button type='submit' disabled={!isValid} className='btn btn-primary w-100 mx-auto'>
+                    Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
