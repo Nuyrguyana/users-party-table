e7b2120e0b29c98b433af0799bacf9d0298b3e03
MichaelD <enee.kors@mail.ru>
2022-01-18 14:45:57 +0300

    install yup (commented out code - file loginForm)

--- src/app/components/ui/loginForn.jsx
+++ src/app/components/ui/loginForn.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { validator } from '../../utils/validator';
 import TextField from '../common/form/textField';
 import CheckBoxField from '../common/form/checkBoxField';
+// import * as yup from 'yup';
 
 const LoginForm = () => {
     const [data, setData] = useState({ email: '', password: '', stayOn: false });
@@ -12,6 +13,19 @@ const LoginForm = () => {
             [target.name]: target.value
         }));
     };
+    // const validateScheme = yup.object().shape({
+    //     password: yup
+    //         .string()
+    //         .required('Пароль обязателен для заполнения')
+    //         .matches(/(?=.*[A-Z])/, 'Пароль должен содержать хотя бы одну заглавную букву')
+    //         .matches(/(?=.*[0-9])/, 'Пароль должен содержать хотя бы одну цифру')
+    //         .matches(/(?=.*[!@#$%^&*])/, 'Пароль должен содержать один из специальных символов !@#$%^&*')
+    //         .matches(/(?=.{8,})/, 'Пароль должен состоять минимум из 8 символов'),
+    //     email: yup
+    //         .string()
+    //         .required('Электронная почта обязательна для заполнения')
+    //         .email('Email введен не корректно')
+    // });
     const validatorConfig = {
         email: {
             isRequired: { message: 'Электронная почта обязательна для заполнения' },
@@ -35,6 +49,10 @@ const LoginForm = () => {
 
     const validate = () => {
         const errors = validator(data, validatorConfig);
+        // validateScheme
+        //     .validate(data)
+        //     .then(() => setErrors({}))
+        //     .catch((err) => setErrors({ [err.path]: err.message }));
         setErrors(errors);
         return Object.keys(errors).length === 0;
     };
